72692fb5d53ec26799c1ecf771a8322dacc0fd49
Trần Duy Thanh <tranduythanh@ssf.vn>
2016-02-28 10:33:05 +0700

    Accomplist pre-work

---
+++ app/src/main/java/com/codepath/simpletodo/MainActivity.java
@@ -0,0 +1,149 @@
+package com.codepath.simpletodo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> todoItems;
+    ArrayAdapter<String> aTodoAdapter;
+    ListView lvItems;
+    EditText etEditText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        setTitle("Simple Todo");
+
+        populateArrayItems();
+        lvItems = (ListView) findViewById(R.id.lvItems);
+        lvItems.setAdapter(aTodoAdapter);
+        etEditText = (EditText) findViewById(R.id.etEditText);
+        lvItems.setOnItemLongClickListener(
+                new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                int itemPosition = position;
+                launchEditItemActivity(itemPosition);
+                return true;
+            }
+        });
+    }
+
+    public void populateArrayItems() {
+        todoItems = new ArrayList<String>();
+        readItems();
+        aTodoAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, todoItems);
+    }
+
+    private void readItems() {
+        File filesDir = getFilesDir();
+        File todoFile = new File(filesDir, "todo.txt");
+        try {
+            todoItems = new ArrayList<String>(FileUtils.readLines(todoFile));
+        } catch (IOException e){
+
+        }
+    }
+
+    private void writeItems() {
+        File filesDir = getFilesDir();
+        File todoFile = new File(filesDir, "todo.txt");
+        try {
+            FileUtils.writeLines(todoFile, todoItems);
+        } catch (IOException e){
+
+        }
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void onAddItem(View view) {
+        String newItem = etEditText.getText().toString();
+        if (checkDuplicateItem(newItem) == false) {
+            aTodoAdapter.add(newItem);
+            writeItems();
+        }
+        etEditText.setText("");
+    }
+
+    public boolean checkDuplicateItem(String newItem){
+        for (String item : todoItems) {
+            if (item.equals(newItem)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private final int REQUEST_CODE = 20;
+    public void launchEditItemActivity(int itemPosition) {
+        // first parameter is the context, second is the class of the activity to launch
+        Intent i = new Intent(MainActivity.this, EditItemActivity.class);
+        i.putExtra("itemContent", todoItems.get(itemPosition));
+        i.putExtra("itemPosition", itemPosition);
+        startActivityForResult(i, REQUEST_CODE); // brings up the second activity
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // REQUEST_CODE is defined above
+        if (resultCode == RESULT_OK && requestCode == REQUEST_CODE) {
+            // Extract name value from result extras
+            String newItemContent = data.getExtras().getString("newItemContent");
+            int itemPosition = data.getExtras().getInt("itemPosition");
+            if (newItemContent.isEmpty()) {
+                todoItems.remove(itemPosition);
+                aTodoAdapter.notifyDataSetChanged();
+            } else {
+                todoItems.set(itemPosition, newItemContent);
+                aTodoAdapter.notifyDataSetChanged();
+            }
+            writeItems();
+
+        }
+    }
+}
